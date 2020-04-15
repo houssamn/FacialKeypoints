58fd8615e4a2851f2abe8ac1585c0b6cebc96c56
Houssam <houssam.naous1@gmail.com>
2020-04-15 22:33:30 +0200

    Added Preliminary Model

--- models.py
+++ models.py
@@ -24,14 +24,24 @@ class Net(nn.Module):
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
-
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.fc1 = nn.Linear(64*53*53, 500)
+        self.drop = nn.Dropout(p=0.4)
+        self.fc2 = nn.Linear(500, 2*68) # size of output
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        # Flatten
+        x = x.view(x.size()[0], -1)
+        x = self.drop(x)
+        x = F.relu(self.fc1(x))
+        x = self.drop(x)
+        x = self.fc2(x)
+
         # a modified x, having gone through all the layers of your model, should be returned
         return x
