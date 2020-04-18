2fe7f057c88b8ef103a11e6ecfdf99c7851864d2
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-04-18 21:10:02 +0000

    created a deeper model

--- models.py
+++ models.py
@@ -13,35 +13,37 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        self.conv1 = nn.Conv2d(1, 32, 4)
         
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        self.fc1 = nn.Linear(64*53*53, 500)
-        self.drop = nn.Dropout(p=0.4)
-        self.fc2 = nn.Linear(500, 2*68) # size of output
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv3 = nn.Conv2d(64, 128, 2)
+        self.conv4 = nn.Conv2d(128, 256, 1)
+        self.drop1 = nn.Dropout(p=0.1)
+        self.drop2 = nn.Dropout(p=0.2)
+        self.drop3 = nn.Dropout(p=0.3)
+        self.drop4 = nn.Dropout(p=0.4)
+        self.drop5 = nn.Dropout(p=0.5)
+        self.drop6 = nn.Dropout(p=0.6)
+        self.fc1 = nn.Linear(256*13*13, 6000)
+        self.fc2 = nn.Linear(6000,1000)
+        self.fc3 = nn.Linear(1000, 2*68) # size of output
+        
         
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.drop(x)
+        x = self.drop1(x)
         x = self.pool(F.relu(self.conv2(x)))
-        # Flatten
-        x = x.view(x.size()[0], -1)
-        x = self.drop(x)
+        x = self.drop2(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.drop3(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.drop4(x)
+        x = x.view(x.size()[0], -1) # Flatten before dense layer
         x = F.relu(self.fc1(x))
-        x = self.drop(x)
-        x = self.fc2(x)
-
-        # a modified x, having gone through all the layers of your model, should be returned
+        x = self.drop5(x)
+        x = F.relu(self.fc2(x))
+        x = self.drop6(x)
+        x = self.fc3(x)
+        
         return x
