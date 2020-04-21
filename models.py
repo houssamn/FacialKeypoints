b4a8ed0c31c0750f96febb6f91003f6fd928eed6
Houssam Naous <houssam.naous1@gmail.com>
2020-04-21 23:08:06 +0000

    experimenting with batch normalization layers

--- models.py
+++ models.py
@@ -13,37 +13,31 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        
+        self.conv1 = nn.Conv2d(1, 32, 7, 2)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 4)
+        self.batch1 = nn.BatchNorm2d(num_features=32, affine=False, track_running_stats=False)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.batch2 = nn.BatchNorm2d(num_features=64, affine=False, track_running_stats=False)
         self.conv3 = nn.Conv2d(64, 128, 3)
-        self.conv4 = nn.Conv2d(128, 256, 2)
-        self.drop1 = nn.Dropout(p=0.1)
-        self.drop2 = nn.Dropout(p=0.2)
-        self.drop3 = nn.Dropout(p=0.3)
-        self.drop4 = nn.Dropout(p=0.4)
+        self.drop3 = nn.Dropout(p=0.2)
         self.drop5 = nn.Dropout(p=0.5)
-        self.drop6 = nn.Dropout(p=0.6)
-        self.fc1 = nn.Linear(256*12*12, 6000)
-        self.fc2 = nn.Linear(6000,1000)
-        self.fc3 = nn.Linear(1000, 2*68) # size of output
+        self.fc1 = nn.Linear(128*11*11, 2048)
+        self.fc2 = nn.Linear(2048,512)
+        self.fc3 = nn.Linear(512, 2*68) # size of output
         
         
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.drop1(x)
+        x = self.batch1(x)
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.drop2(x)
+        x = self.batch2(x)
         x = self.pool(F.relu(self.conv3(x)))
         x = self.drop3(x)
-        x = self.pool(F.relu(self.conv4(x)))
-        x = self.drop4(x)
         x = x.view(x.size()[0], -1) # Flatten before dense layer
         x = F.relu(self.fc1(x))
         x = self.drop5(x)
         x = F.relu(self.fc2(x))
-        x = self.drop6(x)
+        x = self.drop5(x)
         x = self.fc3(x)
         
         return x
