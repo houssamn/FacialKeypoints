10b4a51cbfbd8f39c36ccbfe87f67b25f2035140
Houssam Naous <houssam.naous1@gmail.com>
2020-04-22 23:16:53 +0000

    checkpoint with best model so far

--- models.py
+++ models.py
@@ -1,5 +1,3 @@
-## TODO: define the convolutional neural network architecture
-
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
@@ -13,26 +11,30 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        self.conv1 = nn.Conv2d(1, 32, 7, 2)
+        self.conv1 = nn.Conv2d(1, 8, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        self.batch1 = nn.BatchNorm2d(num_features=32, affine=False, track_running_stats=False)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        self.batch2 = nn.BatchNorm2d(num_features=64, affine=False, track_running_stats=False)
-        self.conv3 = nn.Conv2d(64, 128, 3)
-        self.drop3 = nn.Dropout(p=0.2)
+        self.conv2 = nn.Conv2d(8, 8, 4)
+        self.conv3 = nn.Conv2d(8, 16, 3)
+        self.conv4 = nn.Conv2d(16, 24, 2)
+        self.drop1 = nn.Dropout(p=0.1)
+        self.drop2 = nn.Dropout(p=0.2)
+        self.drop3 = nn.Dropout(p=0.3)
+        self.drop4 = nn.Dropout(p=0.4)
         self.drop5 = nn.Dropout(p=0.5)
-        self.fc1 = nn.Linear(128*11*11, 2048)
-        self.fc2 = nn.Linear(2048,512)
-        self.fc3 = nn.Linear(512, 2*68) # size of output
+        self.fc1 = nn.Linear(24*12*12, 512)
+        self.fc2 = nn.Linear(512,256)
+        self.fc3 = nn.Linear(256, 2*68)
         
         
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.batch1(x)
+        x = self.drop1(x)
         x = self.pool(F.relu(self.conv2(x)))
-        x = self.batch2(x)
+        x = self.drop2(x)
         x = self.pool(F.relu(self.conv3(x)))
         x = self.drop3(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.drop4(x)
         x = x.view(x.size()[0], -1) # Flatten before dense layer
         x = F.relu(self.fc1(x))
         x = self.drop5(x)
